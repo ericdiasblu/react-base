b83234aa6c115f0121581f7fa52af9638013cf9e
Eric Dias <ericdiasblu@gmail.com>
2026-02-01 17:10:22 -0300

    feat: basic css header

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,8 @@
-import { useState, useEffect } from "react";
-import { InputAdd } from "./components/InputAdd";
-import { TodoItem } from "./components/TodoItem";
-import { List } from "./components/List";
-import { TodoAPI, type ITodo } from "./shared/services/api/TodoAPI";
+import { Home } from "./pages/Home";
+import { AppLayout } from "./shared/layout/AppLayout";
 
 export function App() {
-  const [list, setList] = useState<ITodo[]>([]);
-
-  useEffect(() => {
-    TodoAPI.getAll().then((data) => setList(data));
-  }, []);
-
-  const handleAdd = (value: string) => {
-    TodoAPI.create({ label: value, complete: false }).then((data) => {
-      setList([...list, data]);
-    });
-  };
-
-  const handleRemove = (id: string) => {
-    TodoAPI.deleteById(id).then(() => {
-      setList([...list.filter(item => item.id !== id)]);
-    });
-  };
-
-
-  const handleComplete = (id: string) => {
-    TodoAPI.updateById(id, { complete: true }).then(() => {
-      setList([
-        ...list.map(item => ({
-          ...item,
-          complete: item.id === id ? true : item.complete,
-        })),
-      ]);
-    });
-  };
-
-
-  return (
-    <div>
-      <InputAdd onAdd={handleAdd} />
-
-      <List>
-        {list.map((listItem) => (
-          <TodoItem
-            key={listItem.id}
-            onComplete={() => handleComplete(listItem.id)}
-            onRemove={() => handleRemove(listItem.id)}
-            id={listItem.id}
-            label={listItem.label}
-            complete={listItem.complete}
-          />
-        ))}
-      </List>
-    </div>
-  );
+  return <AppLayout>
+    <Home/>
+  </AppLayout>
 }
